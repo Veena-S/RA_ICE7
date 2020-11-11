5ab1ceef9cc4d2a9ced0cc5f75e95f466691b1e1
jeremylim91 <jeremylim_91@live.com>
2020-11-11 11:37:31 +0800

    add jeremys comments

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import { writeFile } from 'fs';
 let name = process.argv[2];
 console.log(`yo ${name}, what's up dude.`);
 console.log(`good day ${name}, are you well?`);
+console.log('hello jeremy added this');
 
 writeFile('data.json', name, error => console.log('err ->',error) )
