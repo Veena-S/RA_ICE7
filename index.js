c70f946f9f9ffd8de5dc0ad380e966f91e24b264
jcleow <jc.leow94@gmail.com>
2020-11-11 11:30:00 +0800

    Added a new line 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { writeFile } from 'fs';
 
 let name = process.argv[2];
+console.log(`yo ${name}, what's up dude.`);
 console.log(`good day ${name}, are you well?`);
 
 writeFile('data.json', name, error => console.log('err ->',error) )
